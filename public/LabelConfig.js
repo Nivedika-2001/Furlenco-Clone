ce95db06a448702a3a41be5dcd569cd4bded6abd
Nivedika-2001 <92134709+Nivedika-2001@users.noreply.github.com>
2025-02-04 21:48:32 +0530

    public folder added

---
+++ public/LabelConfig.js
@@ -0,0 +1,64 @@
+LabelConfig = {
+  header: {
+    buy: "Buy",
+    rent: "Rent",
+    unlmted: "UNLMTD",
+    searchPlaceholder: "What are you searching for?",
+    helloUser: "Hello!",
+    addProduct: "Add Products",
+    viewProducts: "View Products",
+    wishlist: "Wishlist",
+    contactUs: "Contact Us",
+    helpCenter: "Help Center",
+    logout: "Logout",
+  },
+  footer: {
+    description:
+      "Furlenco is an online rental platform offering furniture and home appliance on subscription. Its catalog includes beds, sofas, recliners, fridges, television, wardrobe, and more. It claims to offer customized packages of appliances and furniture and also provides in-house designed products.",
+    navigation: {
+      home: "Home",
+      buyFurniture: "Buy Furniture",
+      rentFurniture: "Rent Furniture",
+      annualReturns: "Annual Returns",
+      aboutUs: "About Us",
+      privacyPolicy: "Privacy Policy",
+      termsAndConditions: "Terms and Conditions",
+    },
+    help: {
+      needHelp: "NEED HELP?",
+      helpCenter: "Help Center",
+    },
+  },
+  deliveryLocation: {
+    buttonText: "Delivery to",
+    enterPincodePlaceholder: "Enter your pincode",
+    selectCity: "Or select your city",
+    currentlySelectedPincode: "Currently selected pincode:",
+    selectDeliveryLocation: "Select Delivery Location",
+    invalidPincodeError: "Invalid Pincode Entered",
+  },
+  Buy: {
+    title: "BUY FURNITURE",
+    categories: [
+      { image: "buy1.webp", text: "Living Room", link: "/living-room" },
+      { image: "buy2.webp", text: "Bedroom", link: "/bedroom" },
+      { image: "buy3.webp", text: "Storage", link: "/storage" },
+      { image: "buy4.webp", text: "Study", link: "/study" },
+      { image: "buy5.webp", text: "Dining", link: "/dining" },
+      { image: "buy6.webp", text: "Tables", link: "/tables" },
+      { image: "buy7.webp", text: "Chairs", link: "/chairs" },
+    ],
+  },
+  rent: {
+    title: "RENT FURNITURE",
+    categories: [
+      { image: "buy2.webp", text: "Bedroom", link: "/bedroom" },
+      { image: "buy1.webp", text: "Living Room", link: "/livingRoom" },
+      { image: "rent1.webp", text: "Appliances", link: "/appliances" },
+      { image: "buy3.webp", text: "Storage", link: "/storage" },
+      { image: "buy4.webp", text: "Study", link: "/study" },
+      { image: "buy6.webp", text: "Dining", link: "/dining" },
+      { image: "rent2.webp", text: "Deals & Combos", link: "/deals" },
+    ],
+  },
+};
